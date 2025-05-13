c4fae8cf3b2b5533a5aee602e49d843181e9d70f
rafael-de-almeida <rafael.almeida.ofical04@gmail.com>
2025-05-13 15:58:38 -0300

    meu deus do céu

--- src/main/java/com/projeto/Ecommerce/dto/FinalizarCompraRequest.java
+++ src/main/java/com/projeto/Ecommerce/dto/FinalizarCompraRequest.java
@@ -3,10 +3,13 @@ package com.projeto.Ecommerce.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class FinalizarCompraRequest {
     private Integer clienteId;
     private Double valorTotal;
-    private Long cupomId;
+    private List<Long> cuponsIds;
+
 }
